d867e1b2df84287329f67aea8bf6e4ef36f2d964
georgiybykov <zhora.bykov@gmail.com>
2020-04-11 23:42:23 +0300

    2nd time migration add references between tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_010623) do
+ActiveRecord::Schema.define(version: 2020_04_11_013149) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "question_id", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -29,12 +31,16 @@ ActiveRecord::Schema.define(version: 2020_04_11_010623) do
     t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "test_id", null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "results", force: :cascade do |t|
     t.integer "score", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "answer_id", null: false
+    t.index ["answer_id"], name: "index_results_on_answer_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -43,6 +49,8 @@ ActiveRecord::Schema.define(version: 2020_04_11_010623) do
     t.boolean "presence", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +60,9 @@ ActiveRecord::Schema.define(version: 2020_04_11_010623) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "results", "answers"
+  add_foreign_key "tests", "categories"
 end
