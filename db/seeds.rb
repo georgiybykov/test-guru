9f3773fd7c7bfd47e6c0c0fc4f8c37f631be8ce5
georgiybykov <zhora.bykov@gmail.com>
2020-04-15 01:06:01 +0300

    new seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,39 +5,34 @@ categories = Category.create!([
 )
 
 tests = Test.create!([
-  { title: 'HTML', level: 0, presence: true, category_id: categories[1].id },
-  { title: 'Ruby', level: 1, presence: true, category_id: categories[0].id },
-  { title: 'Rails', level: 2, presence: true, category_id: categories[0].id },
-  { title: 'Visual Studio', level: 1, presence: true, category_id: categories[2].id },
-  { title: 'Java', level: 5, presence: true, category_id: categories[0].id }
+  { title: 'Beginner', level: 0, presence: true, category_id: categories[1].id },
+  { title: 'Middle', level: 1, presence: true, category_id: categories[0].id }
 ])
 
 questions = Question.create!([
-  { body: 'Is it frontend?', test_id: tests[0].id },
-  { body: 'Is it backend?', test_id: tests[1].id },
-  { body: 'Is it backend?', test_id: tests[2].id },
-  { body: 'Is it user experience?', test_id: tests[3].id },
-  { body: 'Is it backend?', test_id: tests[4].id }
+  { body: 'Is Ruby language for backend development?', test_id: tests[0].id },
+  { body: 'Is Vue.js framework on Ruby language?', test_id: tests[0].id },
+  { body: 'Does Rails contain find_each() method?', test_id: tests[1].id },
+  { body: 'Does full stack consist of backend and frontend?', test_id: tests[1].id },
+  { body: 'Had Ruby on Rails been created for design applications?', test_id: tests[1].id }
 ])
 
 answers = Answer.create!([
-  { body: 'Yes', correct: true, question_id: questions[0].id },
-  { body: 'No', question_id: questions[1].id },
+  { body: 'No', correct: true, question_id: questions[0].id },
+  { body: 'No', correct: true, question_id: questions[1].id },
   { body: 'Yes', correct: true, question_id: questions[2].id },
   { body: 'No', question_id: questions[3].id },
-  { body: 'Yes', correct: true, question_id: questions[4].id }
+  { body: 'Yes', question_id: questions[4].id }
 ])
 
-results = Result.create!([
-  { score: 1, answer_id: answers[0].id },
-  { score: 0, answer_id: answers[1].id },
-  { score: 1, answer_id: answers[2].id },
-  { score: 0, answer_id: answers[3].id },
-  { score: 1, answer_id: answers[4].id }
-])
-
-user = User.create!([
+users = User.create!([
   { name: 'TestName', email: 'test@email.com', password_digest: 'TestPassword' },
   { name: 'TestName1', email: 'test1@email.com', password_digest: 'TestPassword1' },
   { name: 'TestName2', email: 'test2@email.com', password_digest: 'TestPassword2' }
+])
+
+results = Result.create!([
+  { user_id: users[0].id, test_id: tests[0].id },
+  { user_id: users[1].id, test_id: tests[1].id },
+  { user_id: users[2].id, test_id: tests[1].id }
 ])
