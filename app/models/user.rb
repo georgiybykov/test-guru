2549f3918297dcec4084d20f8fe35f3476761e3a
georgiybykov <zhora.bykov@gmail.com>
2020-04-15 01:28:01 +0300

    user_tests_by_level instance method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+
+  def user_tests_by_level(level)
+    Test.joins( "JOIN results ON tests.id = test_id" )
+        .where( level: level, results: { user_id: id } )
+  end
 end
