50686b47d22d33691ae9dfe88a653ff2bbfd3946
georgiybykov <zhora.bykov@gmail.com>
2020-04-15 02:32:05 +0300

    test_titles_by_category_title class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.test_titles_by_category(category_title)
+    joins( "JOIN categories ON categories.id = tests.category_id" )
+    .where( categories: { title: category_title })
+    .order(title: :desc)
+    .pluck(:title)
+  end
 end
