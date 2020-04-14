f3567725d85443edc409ecaa0c586e152b1c69cd
georgiybykov <zhora.bykov@gmail.com>
2020-04-15 01:39:25 +0300

    fix typo in user model method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   def user_tests_by_level(level)
-    Test.joins( "JOIN results ON tests.id = test_id" )
+    Test.joins( "JOIN results ON results.test_id = tests.id" )
         .where( level: level, results: { user_id: id } )
   end
 end
