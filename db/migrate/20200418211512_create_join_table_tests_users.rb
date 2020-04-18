d4a5ff7619eb3914b07caf52e08347fd7204cc97
georgiybykov <zhora.bykov@gmail.com>
2020-04-19 02:16:21 +0300

    fixes in migrations

--- db/migrate/20200418211512_create_join_table_tests_users.rb
+++ db/migrate/20200418211512_create_join_table_tests_users.rb
@@ -1,7 +1,7 @@
 class CreateJoinTableTestsUsers < ActiveRecord::Migration[6.0]
   def change
     create_join_table :users, :tests do |t|
-      t.index [:user_id, :test_id], uniq: true
+      t.index [:user_id, :test_id], unique: true
       # t.index [:test_id, :user_id]
 
       t.timestamps
--- db/migrate/20200418215412_add_column_author_to_tests.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnAuthorToTest < ActiveRecord::Migration[6.0]
-  def change
-    add_column :tests, :author, :bigint, default: nil, null: false
-  end
-end
