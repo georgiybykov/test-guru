f53a209ec528bc73e10d327b7b88150f98460cc7
georgiybykov <zhora.bykov@gmail.com>
2020-04-18 04:57:29 +0300

    tabs on new line to continue long-line methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
 
   def user_tests_by_level(level)
     Test.joins( "JOIN results ON results.test_id = tests.id" )
-        .where( level: level, results: { user_id: id } )
+          .where( level: level, results: { user_id: id } )
   end
 end
