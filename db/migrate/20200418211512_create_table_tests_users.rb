524d8404e68261fbdeb89124281d7012acf1cf7b
georgiybykov <zhora.bykov@gmail.com>
2020-04-19 04:51:14 +0300

    delete unique statement

--- db/migrate/20200418211512_create_table_tests_users.rb
+++ db/migrate/20200418211512_create_table_tests_users.rb
@@ -1,8 +1,8 @@
 class CreateTableTestsUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :tests_users, id: false do |t|
-      t.references :test, null: false, foreign_key: true, index: { unique: true }
-      t.references :user, null: false, foreign_key: true, index: { unique: true }
+      t.references :test, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
