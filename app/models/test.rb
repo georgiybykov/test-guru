dde419b5296d33c738047dcc3b362b6e786d2f23
georgiybykov <zhora.bykov@gmail.com>
2020-04-19 04:02:28 +0300

    add 2 associations: through f_key and join table between tests and users;

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category, optional: true
+  belongs_to :author, class_name: 'User', inverse_of: :authored_tests, foreign_key: :author_id, touch: true
+  has_many :questions, dependant: :destroy
+  has_many :tests_users, dependant: :destroy
+  has_many :users, through: :tests_users
+
   def self.test_titles_by_category(category_title)
     joins( "JOIN categories ON categories.id = tests.category_id" )
       .where( categories: { title: category_title })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :authored_tests, class_name: 'Test', inverse_of: :author, foreign_key: :author_id, dependent: :destroy
+  has_many :tests_users, dependant: :destroy
+  has_many :tests, through: :tests_users
+
   has_secure_password
 
   def user_tests_by_level(level)
