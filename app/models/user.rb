a55827b0e71b0f54a3504f73e3b852288740d013
georgiybykov <zhora.bykov@gmail.com>
2020-04-19 05:02:47 +0300

    change methods in test and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,12 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  def user_tests_by_level(level)
-    Test.joins( "JOIN results ON results.test_id = tests.id" )
-          .where( level: level, results: { user_id: id } )
+  # def show_tests_by_level(level)
+  #   Test.joins( "JOIN results ON results.test_id = tests.id" )
+  #         .where( level: level, results: { user_id: id } )
+  # end
+
+  def show_tests_by_level(level)
+    Test.joins(:tests_users).where(level: level)
   end
 end
