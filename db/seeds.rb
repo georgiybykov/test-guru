803c831a3c1e99f590df3ac5ed7cad0a42827ffa
georgiybykov <zhora.bykov@gmail.com>
2020-04-22 02:40:57 +0300

    new seeds; syntax fix in readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 categories = Category.create!([
-  { title: 'Backend' },
   { title: 'Frontend' },
+  { title: 'Backend' },
   { title: 'UX/UI' }]
 )
 
@@ -11,28 +11,48 @@ users = User.create!([
 ])
 
 tests = Test.create!([
-  { title: 'Beginner', level: 0, presence: true, category: categories[1], author: users[0] },
-  { title: 'Middle', level: 1, presence: true, category: categories[0], author: users[2] }
-])
-
-questions = Question.create!([
-  { body: 'Is Ruby language for backend development?', test: tests[0] },
-  { body: 'Is Vue.js framework on Ruby language?', test: tests[0] },
-  { body: 'Does Rails contain find_each() method?', test: tests[1] },
-  { body: 'Does full stack consist of backend and frontend?', test: tests[1] },
-  { body: 'Had Ruby on Rails been created for design applications?', test: tests[1] }
-])
-
-answers = Answer.create!([
-  { body: 'No', correct: true, question: questions[0] },
-  { body: 'No', correct: true, question: questions[1] },
-  { body: 'Yes', correct: true, question: questions[2] },
-  { body: 'No', question: questions[3] },
-  { body: 'Yes', question: questions[4] }
+  { title: 'Beginner', level: 0, presence: true, category: categories[0], author: users[0] },
+  { title: 'Middle', level: 1, presence: true, category: categories[1], author: users[2] },
+  { title: 'Advanced', level: 2, presence: true, category: categories[2], author: users[1] }
 ])
 
 tests_users = TestsUser.create!([
-  { user: users[0], test: tests[0] },
-  { user: users[1], test: tests[1] },
-  { user: users[2], test: tests[1] }
+  { test: tests[0], user: users[0] },
+  { test: tests[1], user: users[2] },
+  { test: tests[2], user: users[1] }
+])
+
+questions = Question.create!([
+  { body: 'Is Ruby language for backend development?', test: tests[0], answers: [
+    Answer.new(body: 'Yes', correct: true),
+    Answer.new(body: 'No'),
+    Answer.new(body: 'It has been using only for user interface.'),
+    Answer.new(body: 'It is the language for system administration.')
+  ] },
+  { body: 'Is Vue.js framework on Ruby language?', test: tests[0], answers: [
+    Answer.new(body: 'Yes'),
+    Answer.new(body: 'No', correct: true),
+    Answer.new(body: 'It supports all programming languages.')
+  ] },
+  { body: 'Does Rails contain find_each() method?', test: tests[1], answers: [
+    Answer.new(body: 'It is validation method.'),
+    Answer.new(body: 'No'),
+    Answer.new(body: 'Yes', correct: true),
+    Answer.new(body: 'Yes, however if you require special gem.')
+  ] },
+  { body: 'Does full stack consist of backend and frontend development?', test: tests[1], answers: [
+    Answer.new(body: 'It depends of specific framework.'),
+    Answer.new(body: 'No'),
+    Answer.new(body: 'Yes', correct: true)
+  ] },
+  { body: 'Had Ruby on Rails been created for design applications?', test: tests[2], answers: [
+    Answer.new(body: 'It had been created for desingers.'),
+    Answer.new(body: 'No', correct: true),
+    Answer.new(body: 'Yes')
+  ] },
+  { body: 'Could you use JavaScript language in Ruby on Rails framework?', test: tests[2], answers: [
+    Answer.new(body: 'Next question :D'),
+    Answer.new(body: 'No'),
+    Answer.new(body: 'Yes', correct: true)
+  ] }
 ])
