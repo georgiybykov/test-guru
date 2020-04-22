c68f0967948913b7ad8edbf169df5c2443a62df4
georgiybykov <zhora.bykov@gmail.com>
2020-04-22 07:08:02 +0300

    add validation limited_quantity_of_answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,11 @@ class Answer < ApplicationRecord
   belongs_to :question, touch: true
 
   validates :body, presence: true
+  validate :limited_number_of_answers
 
   scope :correct_answer, -> { where(correct: true) }
+
+  def limited_number_of_answers
+    errors.add(:question, 'more than 4 answers') if question.answers.count > 4
+  end
 end
