8144083b5ec4979d8b4a1b0841e36298ba8f8c33
georgiybykov <zhora.bykov@gmail.com>
2020-04-22 18:24:23 +0300

    add unless statement in custom validation; add ON option in validate

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,11 +2,11 @@ class Answer < ApplicationRecord
   belongs_to :question, touch: true
 
   validates :body, presence: true
-  validate :limited_quantity_of_answers
+  validate :limited_quantity_of_answers, on: :create
 
   scope :correct_answer, -> { where(correct: true) }
 
   def limited_quantity_of_answers
-    errors.add(:question, 'too much answers (1..4)') if question.answers.count > 4
+    errors.add(:question, 'too much answers (1..4)') unless question.answers.count < 4
   end
 end
