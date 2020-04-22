b59bde7bd7e028f3e99a5d986ad6fe29fb7a7785
georgiybykov <zhora.bykov@gmail.com>
2020-04-22 17:31:24 +0300

    fix numericality validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,11 +2,11 @@ class Answer < ApplicationRecord
   belongs_to :question, touch: true
 
   validates :body, presence: true
-  validate :limited_number_of_answers
+  validate :limited_quantity_of_answers
 
   scope :correct_answer, -> { where(correct: true) }
 
-  def limited_number_of_answers
-    errors.add(:question, 'more than 4 answers') if question.answers.count > 4
+  def limited_quantity_of_answers
+    errors.add(:question, 'too much answers (1..4)') if question.answers.count > 4
   end
 end
