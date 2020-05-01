e46d6c2f6c3c48e415c0b94ca2cadc222ce1be89
georgiybykov <zhora.bykov@gmail.com>
2020-05-01 04:35:45 +0300

    view a list of the questions of the test

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,27 @@
 class QuestionsController < ApplicationController
+
+  def index
+    @questions = Test.find(params[:test_id]).questions.all
+
+    # render plain: @questions.inspect
+    render inline: '<%= @questions.inspect %>'
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
