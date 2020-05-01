09b54f7b9638f3ab685dbba539b330e1d8fbdc79
georgiybykov <zhora.bykov@gmail.com>
2020-05-01 04:43:14 +0300

    before_action find_test callback

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,27 +1,33 @@
 class QuestionsController < ApplicationController
+  before_action :find_test
 
   def index
-    @questions = Test.find(params[:test_id]).questions.all
+    @questions = @test.questions.all
 
     # render plain: @questions.inspect
     render inline: '<%= @questions.inspect %>'
   end
 
-  def create
-  end
+  # def create
+  # end
 
-  def new
-  end
+  # def new
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def show
-  end
+  def show; end
 
-  def update
-  end
+  # def update
+  # end
 
-  def destroy
+  # def destroy
+  # end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
   end
 end
