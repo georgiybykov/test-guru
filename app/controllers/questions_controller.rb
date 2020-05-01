fa8f689affb1577cad956eae5f854cd6dddfd278
Georgiy Bykov <zhora.bykov@gmail.com>
2020-05-01 07:48:04 +0300

    Update app/controllers/questions_controller.rb
    Co-authored-by: max-underthesun <max.underthesun@gmail.com>

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    @questions = @test.questions.all
+    @questions = @test.questions
 
     render plain: @questions.inspect
     # render inline: '<%= @questions.inspect %>'
