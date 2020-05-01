25479317cb31f870fff41fc5ac258972d4ce9b79
georgiybykov <zhora.bykov@gmail.com>
2020-05-01 04:59:00 +0300

    view body of one question ; add callback find_question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,12 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
+  before_action :find_test, only: %i[index]
+  before_action :find_question, only: %i[show]
 
   def index
     @questions = @test.questions.all
 
-    # render plain: @questions.inspect
-    render inline: '<%= @questions.inspect %>'
+    render plain: @questions.inspect
+    # render inline: '<%= @questions.inspect %>'
   end
 
   # def create
@@ -17,7 +18,9 @@ class QuestionsController < ApplicationController
   # def edit
   # end
 
-  def show; end
+  def show
+    render plain: @question.inspect
+  end
 
   # def update
   # end
@@ -29,5 +32,9 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end
