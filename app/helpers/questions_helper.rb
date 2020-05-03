c7b7aadb4bd8eca1d601e9d8aa4cec2dca320402
georgiybykov <zhora.bykov@gmail.com>
2020-05-03 08:08:44 +0300

    add question_header helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_header(question)
+    if action_name.to_sym == :new
+      "Create New #{question.test.title} Question"
+    elsif action_name.to_sym == :edit
+      "Edit #{question.test.title} Question"
+    end
+  end
 end
