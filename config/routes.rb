41c35585cc1f97dfe0ec82fab5025b7272505857
georgiybykov <zhora.bykov@gmail.com>
2020-05-07 07:49:55 +0300

    create test_passages controller; add routes to start & result resources

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
     end
+
+    post :start, on: :member
+  end
+
+  resources :test_passages, only: %i[show update] do
+    get :result, on: :member
   end
 end
