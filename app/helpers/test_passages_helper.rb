b98d081cdbd36ff1c7aea52653dd5a07c2743937
georgiybykov <zhora.bykov@gmail.com>
2020-05-08 07:50:17 +0300

    move logic success_failure from result_view to helper

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -8,6 +8,10 @@ module TestPassagesHelper
     test_passage.success? ? TEST_PASSAGE[:success] : TEST_PASSAGE[:failure]
   end
 
+  def success_failure(test_passage)
+    test_passage.success? ? 'success' : 'failure'
+  end
+
   def question_number(test_passage)
     "The Question #{test_passage.current_question_number} of #{test_passage.questions_count}."
   end
