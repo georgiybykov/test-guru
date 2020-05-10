12da8b80321e2ef1ac5770ef4056f2c2fd04ccda
georgiybykov <zhora.bykov@gmail.com>
2020-05-10 20:37:38 +0300

    add authenticate mthd to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
   has_many :authored_tests, class_name: 'Test', inverse_of: :author, foreign_key: :author_id, dependent: :destroy
   has_many :test_passages, dependent: :destroy
@@ -15,5 +17,15 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
+  end
+
+  def authenticate(password_digest)
+    digest(password_digest) == self.password_digest ? self : false
+  end
+
+  private
+
+  def digest(string)
+    Digest::SHA1.hexdigest(string)
   end
 end
