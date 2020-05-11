c1bef91e93e44c738a764d83ee6a4ebf52d1d6d4
georgiybykov <zhora.bykov@gmail.com>
2020-05-11 03:58:35 +0300

    create show_flash_message helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to repo, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def show_flash_message
+    content_tag :p, flash[:alert], class: 'flash alert' if flash[:alert]
+  end
 end
