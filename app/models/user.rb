5cc65a6ed9c7d066891e1de0f131102f613e4a73
georgiybykov <zhora.bykov@gmail.com>
2020-05-11 22:50:53 +0300

    delete redundant validations for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ApplicationRecord
-  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]{2,}+\z/i.freeze
+  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
   has_many :authored_tests, class_name: 'Test', inverse_of: :author, foreign_key: :author_id, dependent: :destroy
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
-  validates :name, presence: true, length: { within: 2..20 }
-  validates :email, presence: true, uniqueness: true, length: { within: 4..30 }
-  validates :email, format: { with: EMAIL_FORMAT }
-  validates :password, presence: true, length: { minimum: 6 }, if: Proc.new { |u| u.password_digest.blank? }
-  validates :password, confirmation: true
+  validates :name, presence: true, length: { within: 2..40 }
+  validates :email, presence: true,
+                    length: { within: 4..50 },
+                    format: { with: EMAIL_FORMAT },
+                    uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
 
   has_secure_password
 
