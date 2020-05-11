933540d56dd2439cfd0e55f66ab2d83ac6a6337c
georgiybykov <zhora.bykov@gmail.com>
2020-05-11 05:45:25 +0300

    validation format & uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
+  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]{2,}+\z/i.freeze
+
   has_many :authored_tests, class_name: 'Test', inverse_of: :author, foreign_key: :author_id, dependent: :destroy
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
   validates :name, presence: true, length: { within: 2..20 }
-  validates :email, presence: true, length: { within: 4..30 }
+  validates :email, presence: true, uniqueness: true, length: { within: 4..30 }
+  validates :email, format: { with: EMAIL_FORMAT }
   validates :password, presence: true, length: { minimum: 6 }, if: Proc.new { |u| u.password_digest.blank? }
   validates :password, confirmation: true
 
