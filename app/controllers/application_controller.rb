a89e26766c11c68902ec41b539c2a2e97a1464c8
georgiybykov <zhora.bykov@gmail.com>
2020-05-14 02:55:36 +0300

    redirect user to root_path || admin/tests if admin?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  protected
+
+  def after_sign_in_path_for(user)
+    current_user.admin? ? admin_tests_path : stored_location_for(:user) || root_path
+  end
+
+  # def after_sign_in_path_for(user)
+  #   current_user.admin? ? admin_tests_path : super
+  # end
 end
