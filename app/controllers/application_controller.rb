36584d005077375fb32645233d8fc5cdecf53f5f
georgiybykov <zhora.bykov@gmail.com>
2020-05-14 03:35:53 +0300

    add first & last name attributes to user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protect_from_forgery with: :exception
 
   protected
@@ -10,4 +12,9 @@ class ApplicationController < ActionController::Base
   # def after_sign_in_path_for(user)
   #   current_user.admin? ? admin_tests_path : super
   # end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys: [:first_name, :last_name]
+    devise_parameter_sanitizer.permit :account_update, keys: [:first_name, :last_name]
+  end
 end
