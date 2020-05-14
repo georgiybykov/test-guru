3720ae32a55ce413ce04ed6d2cd7379cad18102f
georgiybykov <zhora.bykov@gmail.com>
2020-05-14 05:44:27 +0300

    fix error; admin is test author by user test association

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -16,8 +16,7 @@ class Admin::TestsController < Admin::BaseController
   def edit; end
 
   def create
-    @test = Test.new(test_params)
-    @test.author = current_user.authored_tests
+    @test = current_user.authored_tests.new(test_params)
 
     if @test.save
       redirect_to [:admin, @test], notice: 'Test was successfully created!'
