897a2babfdc604c8dc8283f699d37a5b1cbfa938
georgiybykov <zhora.bykov@gmail.com>
2020-05-16 04:47:24 +0300

    add localization to tests_passage views

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,18 +1,9 @@
 module TestPassagesHelper
-  TEST_PASSAGE = {
-    success: 'You have been completed the Test successfully!',
-    failure: 'You have been failed the Test!'
-  }.freeze
-
   def result_message(test_passage)
-    test_passage.success? ? TEST_PASSAGE[:success] : TEST_PASSAGE[:failure]
+    test_passage.success? ? t('.success') : t('.failure')
   end
 
   def success_failure(test_passage)
     test_passage.success? ? 'success' : 'failure'
-  end
-
-  def question_number(test_passage)
-    "The Question #{test_passage.current_question_number} of #{test_passage.questions_count}."
   end
 end
