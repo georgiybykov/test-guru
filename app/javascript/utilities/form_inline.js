f32d65fa77f237bbc004e77ee7d920f746bacb16
georgiybykov <zhora.bykov@gmail.com>
2020-06-14 19:01:27 +0300

    change some mthds from js to jQuery in form_inline.js

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -1,11 +1,13 @@
 document.addEventListener('turbolinks:load', function() {
-  var controls = document.querySelectorAll('.form-inline-link')
+  $('.form-inline-link').on('click', formInlineLinkHandler)
 
-  if (controls.length) {
-    for (var i = 0; i < controls.length; i++) {
-      controls[i].addEventListener('click', formInlineLinkHandler)
-    }
-  }
+  // var controls = document.querySelectorAll('.form-inline-link')
+
+  // if (controls.length) {
+  //   for (var i = 0; i < controls.length; i++) {
+  //     controls[i].addEventListener('click', formInlineLinkHandler)
+  //   }
+  // }
 
   var errors = document.querySelector('.resource-errors')
 
@@ -24,16 +26,24 @@ function formInlineLinkHandler(event) {
 
 function formInlinekHandler(testId) {
   var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
-  var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
-  var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
+  var $testTitle = $('.test-title[data-test-id="' + testId + '"]')
+  var $formInline = $('.form-inline[data-test-id="' + testId + '"]')
 
-  if (formInline.classList.contains('hide')) {
-    testTitle.classList.add('hide')
-    formInline.classList.remove('hide')
+  // var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
+  // var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
+
+  $testTitle.toggle()
+  $formInline.toggle()
+
+
+  // if (formInline.classList.contains('hide')) {
+  if ($formInline.is(':visible')) {
+    // testTitle.classList.add('hide')
+    // formInline.classList.remove('hide')
     link.textContent = 'Cancel'
   } else {
-    testTitle.classList.remove('hide')
-    formInline.classList.add('hide')
+    // testTitle.classList.remove('hide')
+    // formInline.classList.add('hide')
     link.textContent = 'Edit'
   }
 }
