fce77c140b82239078aabf63a2dd7e71bb35d472
georgiybykov <zhora.bykov@gmail.com>
2020-07-05 18:28:13 +0300

    fix db config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_guru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://lovely-arches-92588.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'lovely-arches-92588.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     adress:                              'smtp.gmail.com',
