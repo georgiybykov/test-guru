db67f1853a2d684dc4b1fe0c0f01be444969a0d5
georgiybykov <zhora.bykov@gmail.com>
2020-10-26 00:23:59 +0300

    Change SMTP auth settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    address:                              'smtp.gmail.com',
-    port:                                 587,
+    address:                              'smtp.mail.ru',
+    port:                                 465,
     user_name:                            ENV['SMTP_USERNAME'],
     password:                             ENV['SMTP_PASSWORD'],
     authentication:                       'plain',
