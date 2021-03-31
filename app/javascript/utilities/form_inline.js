67fabe79f931730022e889a4a8bc26c1eed11d67
georgiybykov <zhora.bykov@gmail.com>
2021-04-01 00:51:57 +0300

    Fix js function variable naming

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -14,7 +14,7 @@ document.addEventListener('turbolinks:load', function() {
 
   if (($controls) && ($controls.length != 0) && (errors)) {
     var resourceId = errors.dataset.resourceId
-    formInlinekHandler(resourceId)
+    formInlineHandler(resourceId)
   }
 })
 
@@ -22,10 +22,10 @@ function formInlineLinkHandler(event) {
   event.preventDefault()
 
   var testId = this.dataset.testId
-  formInlinekHandler(testId)
+  formInlineHandler(testId)
 }
 
-function formInlinekHandler(testId) {
+function formInlineHandler(testId) {
   var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
   var $testTitle = $('.test-title[data-test-id="' + testId + '"]')
   var $formInline = $('.form-inline[data-test-id="' + testId + '"]')
